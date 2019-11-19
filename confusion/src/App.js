cb886d1967946f77f83522544d7ae71b32c2373c
Evan Genest <genest.1@osu.edu>
2019-11-18 19:35:06 -0600

    Add logo artwork to navbar.

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,7 +2,8 @@ import  React, { Component } from 'react';
 import Menu from './components/MenuComponent';
 import {
     Navbar,
-    NavbarBrand
+    NavbarBrand,
+    Media
 } from 'reactstrap';
 
 const thought1 = "Choose high-leverage activities"
@@ -13,11 +14,20 @@ class App extends Component {
         return (
  
       <div className="App">
-        <Navbar dark color="primary">
+        <Navbar dark color="danger">
           <div className="container">
+
+      <Media left href="#">
+        <Media object src="assets/images/logo.png" alt="Logo for con Fusion restaurant" />
+      </Media>
+
+
+
+
             <NavbarBrand href="/">{thought1} and {thought1}</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
 
 
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -6,7 +6,7 @@ class Menu extends Component {
         super(props);
         this.state = {
             dishes: [
-                {
+                { 
                   id: 0,
                   name:'Uthappizza',
                   image: 'assets/images/uthappizza.png',
@@ -45,7 +45,7 @@ class Menu extends Component {
     render() {
         const menu = this.state.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 mt-5">
+              <div key={dish.id} className="col-12 mt-5"> 
                 <Media tag="li">
                   <Media left middle>
                       <Media object src={dish.image} alt={dish.name} />
