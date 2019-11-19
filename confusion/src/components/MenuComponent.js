0dc97dd9b7a9b9cd4ce4ff83e04b5b753c0ccd79
Evan Genest <genest.1@osu.edu>
2019-11-18 22:55:37 -0600

    Make just a generic card at the bottom.

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -2,30 +2,50 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTxt, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
-    constructor(props){
-      super(props);
-      this.state = {};
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        }
     }
 
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div key={dish.id} className="col-12 col-md-5 m-1"> 
+    onDishSelect(dish){
+      this.setState({selectedDish: dish})
+    }
 
-                <Card>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                    <CardTitle>{dish.name} </CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+
+
+      renderDish(dish) {
+        if (dish != null) {
+          console.log('looks good')
+        } else {
+          console.log('looks good')
+        }
+      }
+
+    render() {
+      const menu = this.props.dishes.map((dish) => {
+        return (
+          <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card onClick={() => console.log('yes!')}>
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                <CardTitle>{dish.name} </CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </div>
             );
-        });
+        })
+
+
 
         return (
-          <div className="container">
+            <div className="container">
             <div className="row">
                   {menu}
+            </div>
+            <div className="row">
+                  {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
